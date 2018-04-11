0c5156b2e187fd8da231bc3f4045cf5a4f07cf2b
Arber Xhindoli <axhindol@umich.edu>
2018-04-11 01:04:43 -0400

    began

---
+++ summarize.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+'''
+Summarize application made for EECS486 Project 4
+'''
+
+import sys
+import os
+import datetime
+import math
+from random import randint
+from porterstemmer import PorterStemmer
+
+
+class Sentence(object):
+    '''
+    Class contains:
+        original sentence (string)
+        preprocessed list of tokens
+        sentence score (float)
+    '''
+    def __init__(self):
+        '''initalize members'''
+
+
+def main(args):
+    '''Input is file that text we want to summarize'''
+
+
+if __name__ == '__main__':
+    '''Run through the command line take in arguments'''
+    main(sys.argv[1:])
